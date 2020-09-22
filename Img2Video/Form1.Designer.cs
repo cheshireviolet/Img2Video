3153e3ba34923d4f2ad67379a8b941609d443f42
cheshireviolet <cursedzerow@gmail.com>
2020-09-22 00:19:15 -0300

    Add files via upload
    1.0 initial release

---
+++ Img2Video/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Img2Video
+{
+    partial class Screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Screen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.imgType = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this._resX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this._resY = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.fps = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this._text = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this._textY = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this._textX = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this._textColor = new System.Windows.Forms.ComboBox();
+            this._textFont = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this._fontSize = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this._textBorder = new System.Windows.Forms.ComboBox();
+            this._song = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // imgType
+            // 
+            resources.ApplyResources(this.imgType, "imgType");
+            this.imgType.Name = "imgType";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // _resX
+            // 
+            resources.ApplyResources(this._resX, "_resX");
+            this._resX.Name = "_resX";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // _resY
+            // 
+            resources.ApplyResources(this._resY, "_resY");
+            this._resY.Name = "_resY";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // fps
+            // 
+            resources.ApplyResources(this.fps, "fps");
+            this.fps.Name = "fps";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // _text
+            // 
+            resources.ApplyResources(this._text, "_text");
+            this._text.Name = "_text";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // _textY
+            // 
+            resources.ApplyResources(this._textY, "_textY");
+            this._textY.Name = "_textY";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // _textX
+            // 
+            resources.ApplyResources(this._textX, "_textX");
+            this._textX.Name = "_textX";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // _textColor
+            // 
+            this._textColor.FormattingEnabled = true;
+            resources.ApplyResources(this._textColor, "_textColor");
+            this._textColor.Name = "_textColor";
+            // 
+            // _textFont
+            // 
+            resources.ApplyResources(this._textFont, "_textFont");
+            this._textFont.Name = "_textFont";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // _fontSize
+            // 
+            resources.ApplyResources(this._fontSize, "_fontSize");
+            this._fontSize.Name = "_fontSize";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // _textBorder
+            // 
+            this._textBorder.FormattingEnabled = true;
+            resources.ApplyResources(this._textBorder, "_textBorder");
+            this._textBorder.Name = "_textBorder";
+            // 
+            // _song
+            // 
+            resources.ApplyResources(this._song, "_song");
+            this._song.Name = "_song";
+            this._song.UseVisualStyleBackColor = true;
+            // 
+            // Screen
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._song);
+            this.Controls.Add(this._textBorder);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this._fontSize);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this._textFont);
+            this.Controls.Add(this._textColor);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this._textY);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this._textX);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this._text);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fps);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this._resY);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this._resX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.imgType);
+            this.Controls.Add(this.label1);
+            this.Name = "Screen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox imgType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _resX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox _resY;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox fps;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox _text;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox _textY;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox _textX;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox _textColor;
+        private System.Windows.Forms.TextBox _textFont;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox _fontSize;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ComboBox _textBorder;
+        private System.Windows.Forms.CheckBox _song;
+    }
+}
+
---
+++ Img2Video/Form1.cs
@@ -0,0 +1,106 @@
+﻿/*Made by Cecília Takeda using ffmpeg
+* 21/09/2020 Mom I love you thank you 
+*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Img2Video
+{
+
+    public partial class Screen : Form
+    {
+
+        public Screen()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //sorry for this string lol
+            string _colors = "Aqua,Azure,Beige,Black,Blue,Brown,Crimson,Cyan,DeepPink,ForestGreen,Fuchsia,Gold," +
+                "Gray,Green,HotPink,Indigo,Ivory,Magenta,Maroon,Navy,Orange,Pink,Purple,Red,Salmon,Silver,SkyBlue," +
+                "Snow,SteelBlue,Teal,Tomato,Turquoise,Violet,White,Yellow";
+            _textColor.Items.AddRange( _colors.Split(',').ToArray());
+            _textColor.SelectedItem = "Black";
+            _textBorder.Items.Add("Black");
+            _textBorder.Items.Add("White");
+            _textBorder.Items.Add("No Border");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //button1.Hide();
+
+            //FPS + input file
+            bool _vf = false;
+            string _input = "-y -framerate " + fps.Text + " -i img\\%03d." + imgType.Text;
+            //song
+            if (_song.Checked) _input += " -i song.mp3 -shortest";
+            //Resolution
+            if (_resX.Text != "" && _resY.Text != "")
+            {
+                _vf = true;
+                _input += " -vf \"scale=" + _resX.Text + ":" + _resY.Text;
+            }
+            //Text
+            if (_text.Text != "")
+            {
+                //it shouldn't -vf twice and should : in case scaling
+                _input += (!_vf) ? " -vf \"drawtext=" : ":drawtext=";                 
+                _vf = false;
+                //bordercolor
+                switch(_textBorder.SelectedIndex)
+                {
+                    case 0:
+                        _input += "borderw=2:";
+                        break;
+                    case 1:
+                        _input += "borderw=2:bordercolor=white:";
+                        break;
+                    default:
+                        break;
+                }
+                //text color
+                _input += "fontcolor=" + _textColor.Text + ":";
+                //font
+                _input += "font=" + _textFont.Text + ":";
+                //fontsize
+                _input += "fontsize=" + _fontSize.Text + ":";
+                //text
+                _input += "text='" + _text.Text + "':";
+                //XY
+                if (_textX.Text == "" && _textY.Text == "") _input += "x=(w-text_w)/2:y=(h-text_h-line_h)/2\"";
+                else _input += "x=" + _textX.Text + ":y=" + _textY.Text + "\"";
+
+            }
+            if (_vf) _input += "\"";
+            _input += " -c:v h264 -r " + fps.Text + " video.mp4";
+
+            Process _ffmpeg = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "ffmpeg.exe",
+                    Arguments = _input,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardInput = true,
+                }
+            };
+            _ffmpeg.Start();
+            _ffmpeg.WaitForExit();
+            MessageBox.Show("Success", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+
+    }
+
+   
+}
